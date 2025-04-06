eaaf62470c7e567f680fe504a7296089c0398332
Rtemmm <rtemmm4@gmail.com>
2025-04-06 17:05:46 +0500

    мина

---
+++ Zombie picker/Assets/Scripts/Mines/Mine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    private void Update()
+    {
+        if (transform.position.x >= 20) 
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<Zombie>().TakeDamage(200);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Zombie picker/Assets/Scripts/Mines/MineSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class MineSpawner : MonoBehaviour
+{
+    public GameObject minePrefab;
+    public float mineOffset = 5f;
+    
+    private void SpawnMines()
+    {
+        var lastPosition = new Vector3(transform.position.x, 0f, 0f);
+        
+        for (var i = 0; i < 10; i++)
+        {
+            Instantiate(minePrefab, lastPosition, Quaternion.Euler(0, 90, 0));
+            lastPosition -= new Vector3(mineOffset, 0f, 0f);
+        }
+        
+        Destroy(gameObject);
+    }
+}
