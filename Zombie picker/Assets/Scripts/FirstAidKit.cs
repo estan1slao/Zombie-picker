00e2a09f940306ba712419adc71a7359e63d0b93
Rtemmm <rtemmm4@gmail.com>
2025-04-06 16:18:06 +0500

    move forward

--- Zombie picker/Assets/Scripts/FirstAidKit.cs
+++ Zombie picker/Assets/Scripts/FirstAidKit.cs
@@ -2,18 +2,9 @@
 
 public class FirstAidKit : MonoBehaviour
 {
-    public float speed = 5f;
-
     private void Update()
     {
-        MoveForward();
-        
         if (transform.position.x >= 20) 
             Destroy(gameObject);
-    }
-
-    private void MoveForward()
-    {
-        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
     }
 }
---
+++ Zombie picker/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speed = 5f;
+    
+    private void Update()
+    {
+        Move();
+    }
+    
+    private void Move()
+    {
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+    }
+}
