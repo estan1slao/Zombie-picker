683b5521213c227c5dcf3f95645c5939916d117b
Rtemmm <rtemmm4@gmail.com>
2025-04-10 12:03:08 +0500

    фоновые звуки

---
+++ Zombie picker/Assets/Scripts/AmbientSoundController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AmbientSoundController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip menuSound;
+    public AudioClip gameSound;
+
+    public static AmbientSoundController Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        SceneManager.activeSceneChanged += (current, next) =>
+        {
+            audioSource.clip = next.name == "MainMenu" ? menuSound : gameSound;
+            audioSource.Play();
+        };
+    }
+}
