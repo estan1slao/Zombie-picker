8ddd7c67fac46ebe5d4de2021fec940d594dd68e
Rtemmm <rtemmm4@gmail.com>
2025-04-10 12:53:18 +0500

    звук победы поражения

---
+++ Zombie picker/Assets/Scripts/Sound/WinLoseSoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class WinLoseSoundManager : MonoBehaviour
+{
+    public AudioClip sound;
+    public AudioMixerGroup audioMixerGroup;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.outputAudioMixerGroup = audioMixerGroup;
+    }
+
+    private void OnEnable()
+    {
+        audioSource.PlayOneShot(sound);
+    }
+}
