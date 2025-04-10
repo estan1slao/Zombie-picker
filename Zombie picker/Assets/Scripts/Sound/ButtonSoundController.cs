f5ed334688e20bcac128c0b6fc3ec522785ccb88
Rtemmm <rtemmm4@gmail.com>
2025-04-10 13:55:46 +0500

    звуки кнопок

---
+++ Zombie picker/Assets/Scripts/Sound/ButtonSoundController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class ButtonSoundController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public static ButtonSoundController Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+        
+        SceneManager.activeSceneChanged += (current, next) => FindButtons();
+    }
+
+    private void Start() => FindButtons();
+
+    private void FindButtons()
+    {
+        var buttons = FindObjectsOfType<Button>(true);
+        foreach (var button in buttons)
+        {
+            button.onClick.AddListener(PlaySound);
+        }
+    }
+    
+    private void PlaySound()
+    {
+        audioSource.Play();
+    }
+}
